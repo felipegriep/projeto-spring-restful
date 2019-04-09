883018d8758cca9078a828f02184522d0393478c
Felipe Griep <griep.felipe@gmail.com>
2019-04-08 23:40:36 -0300

    Primeiro Commmit

---
+++ src/main/java/com/griep/projetospringrestful/ProjetoSpringRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.griep.projetospringrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griep/projetospringrestful/ProjetoSpringRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.griep.projetospringrestful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoSpringRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
