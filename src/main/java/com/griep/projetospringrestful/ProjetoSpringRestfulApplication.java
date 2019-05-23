ffbf4d9b613de301304036cb79d8cdb1f1839b88
Felipe Griep <griep.felipe@gmail.com>
2019-05-22 23:46:25 -0300

    Encode de senha

--- src/main/java/com/griep/projetospringrestful/ProjetoSpringRestfulApplication.java
+++ src/main/java/com/griep/projetospringrestful/ProjetoSpringRestfulApplication.java
@@ -9,21 +9,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ProjetoSpringRestfulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSpringRestfulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringRestfulApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encodada: " + senhaEncoded);
-			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encodada novamente: " + senhaEncoded);
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+            System.out.println("Senha encodada: " + senhaEncoded);
+            senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+            System.out.println("Senha encodada novamente: " + senhaEncoded);
 
-			boolean senhaValida = SenhaUtils.senhaValida("123456", senhaEncoded);
-			System.out.println("Senha Válida: " + senhaValida);
-		};
-	}
+            boolean senhaValida = SenhaUtils.senhaValida("123456", senhaEncoded);
+            System.out.println("Senha Válida: " + senhaValida);
+        };
+    }
 
 }
